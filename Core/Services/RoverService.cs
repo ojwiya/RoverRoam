92f883b4dc4a744d9a5b064617b63669ac4645f8
ojwiya@gmail.com <ojwiya@gmail.com>
2016-05-19 07:47:40 +0100

    service refactor

--- Core/Services/RoverService.cs
+++ Core/Services/RoverService.cs
@@ -17,95 +17,39 @@ namespace Core.Services
 
         public string[] Roam(string plateauSize, Instruction[] instructions)
         {
-          
-           string[] result = { "" };
         
             // Generate plateau grid -- Array
            GeneratePlateauGrid(plateauSize);
            
-          // Iterate through controllers instructions (update the model after each loop)
-           var rovers = MoveRovers(instructions);
+          // Iterate through rover instructions and execute
+           var rovers = ProcessInstructions(instructions);
 
            return GetFinalPositions(rovers);   
         }
-
-        private string[] GetFinalPositions(Rover[] rovers)
+        
+        private void GeneratePlateauGrid(string plateauSize)
         {
-            string[] positions = new string[rovers.Length];            
-            for (int i = 0; i < rovers.Length; i++)
-			{
-                positions[i] = rovers[i].CurrentPosition.X 
-                                + " " +  rovers[i].CurrentPosition.Y
-                                + " " + rovers[i].Bearing.Abbreviation;
-			}
+            string[] size = plateauSize.Split(' ');
+            int h = Convert.ToInt16(size[0]);
+            int w = Convert.ToInt16(size[1]);
 
-            return positions;
+            _plateau = new int[h,w];
         }
 
-        private Rover[] MoveRovers(Instruction[] instructions)
+       
+        private Rover[] ProcessInstructions(Instruction[] instructions)
         {
 
-            // Interpret input into data structures
-            Rover[] roverInstructions = ParseRoverData(instructions);
+            // Translate input into structured data model
+            Rover[] roverData = PrepareRoverData(instructions);
 
             // Iterate through data structure and update shift rover.
-            ProcessInstructions(roverInstructions);
+            MoveRovers(roverData);
 
-            return roverInstructions;
+            return roverData;
         }
-
-        private void ProcessInstructions(Rover[] roverInstructions)
-        {
-            foreach (var r in roverInstructions)
-            {
-                foreach (var command in r.Instruction)
-                {
-                    Execute(command, r);  
-                }
-                        
-            }
-
-        }
-
-        private void Execute(char command, Rover rover)
-        {
-            //execute instructions
-            switch (command)
-            {
-                case 'L':
-                    rover.Bearing.Degree = validateAngle(rover.Bearing.Degree - 90);
-                    rover.Bearing.Abbreviation = _positionService.ConvertDegreeToBearing(
-                rover.Bearing.Degree); 
-                    break;
-                case 'R':
-                    rover.Bearing.Degree = validateAngle(rover.Bearing.Degree + 90);
-                    rover.Bearing.Abbreviation = _positionService.ConvertDegreeToBearing(
-                                                        rover.Bearing.Degree); 
-                    break;
-                case 'M':
-                    _positionService.SetNewPosition(rover); 
-                    break;
-                default:
-                    break;
-            }
-
-
-        }
-
-        private int validateAngle(int angle)
-        {
-            if (angle > 360) {
-                return angle % 360;
-            }
-            else if (angle <= 0) {
-                return 360-(angle % 360);
-            }
-
-            return angle;
-        }
-
-
-        private Rover[] ParseRoverData(Instruction[] instructions)
+        
+        private Rover[] PrepareRoverData(Instruction[] instructions)
         {
             Rover[] results = new Rover[instructions.Length];
 
@@ -125,6 +69,44 @@ namespace Core.Services
             return results;
         }
 
+        private void MoveRovers(Rover[] roverInstructions)
+        {
+            foreach (var r in roverInstructions)
+            {
+                foreach (var command in r.Instruction)
+                {
+                    Execute(command, r);  
+                }
+                        
+            }
+
+        }
+
+        private void Execute(char command, Rover rover)
+        {
+            //execute instructions
+            switch (command)
+            {
+                case 'L':
+                    rover.Bearing.Degree = ValidateAngle(rover.Bearing.Degree - 90);
+                    rover.Bearing.Abbreviation = _positionService.ConvertDegreeToBearing(
+                rover.Bearing.Degree); 
+                    break;
+                case 'R':
+                    rover.Bearing.Degree = ValidateAngle(rover.Bearing.Degree + 90);
+                    rover.Bearing.Abbreviation = _positionService.ConvertDegreeToBearing(
+                                                        rover.Bearing.Degree); 
+                    break;
+                case 'M':
+                    _positionService.SetNewPosition(rover); 
+                    break;
+                default:
+                    break;
+            }
+
+
+        }
+       
         private Position GetPositionFromInstructions(string startPosition)
         {
             var _startPosition = startPosition.Split(' ');
@@ -144,14 +126,30 @@ namespace Core.Services
                 Degree = _positionService.ConvertBearingToDegree(_startPosition[2])
             };
         }
-
-        private void GeneratePlateauGrid(string plateauSize)
+        
+        private int ValidateAngle(int angle)
         {
-            string[] size = plateauSize.Split(' ');
-            int h = Convert.ToInt16(size[0]);
-            int w = Convert.ToInt16(size[1]);
+            if (angle > 360) {
+                return angle % 360;
+            }
+            else if (angle <= 0) {
+                return 360-(angle % 360);
+            }
 
-            _plateau = new int[h,w];
+            return angle;
+        }
+       
+         private string[] GetFinalPositions(Rover[] rovers)
+        {
+            string[] positions = new string[rovers.Length];            
+            for (int i = 0; i < rovers.Length; i++)
+			{
+                positions[i] = rovers[i].CurrentPosition.X 
+                                + " " +  rovers[i].CurrentPosition.Y
+                                + " " + rovers[i].Bearing.Abbreviation;
+			}
+
+            return positions;
         }
 
     }
